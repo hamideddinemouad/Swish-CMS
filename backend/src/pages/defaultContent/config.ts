ed36ac6ae885adb090f61b18672b5e24d8354eef
mouad <hamideddinemouad@gmail.com>
2026-04-15 17:29:06 +0100

    fix : default image placeholder

--- backend/src/pages/defaultContent/config.ts
+++ backend/src/pages/defaultContent/config.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_IMAGE_PLACEHOLDER_URL =
-  "/upload_placeholder.svg";
+  "https://swish.ltd/upload_placeholder.svg";
 
 const pages = ["about", "articles", "categories", "contact", "faq", "home"];
