c4312fed6b529858ef87cc61b067c07cc8a81542
mouad <hamideddinemouad@gmail.com>
2026-04-15 17:15:00 +0100

    fix : live-tenant-site-button points to proper tenant site

--- backend/src/pages/defaultContent/config.ts
+++ backend/src/pages/defaultContent/config.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_IMAGE_PLACEHOLDER_URL =
-  "http://localhost:4000/upload_placeholder.svg";
+  "/upload_placeholder.svg";
 
 const pages = ["about", "articles", "categories", "contact", "faq", "home"];
